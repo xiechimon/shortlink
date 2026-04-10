eb8956e731f28ef6cb58d5cfc6680fa917c8c2b0
xiechimon <xiechimon@qq.com>
2026-04-10 20:09:29 +0800

    feat(admin): 添加短链接远程控制器与 DTO

---
+++ admin/src/main/java/com/xmon/shortlink/admin/remote/dto/ShortLinkRemoteService.java
@@ -0,0 +1,42 @@
+package com.xmon.shortlink.admin.remote.dto;
+
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.xmon.shortlink.admin.common.convention.result.Result;
+import com.xmon.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.xmon.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.xmon.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.xmon.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+
+import java.util.HashMap;
+
+/**
+ * 管理端调用中台短链接能力的远程服务接口。
+ */
+public interface ShortLinkRemoteService {
+
+    /**
+     * 调用中台的短链接创建接口。
+     */
+    default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam) {
+        String resultBodyStr = HttpUtil.post("http://localhost:8001/api/shortlink/v1/create", JSON.toJSONString(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 调用中台分页接口查询短链接列表。
+     */
+    default Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        HashMap<String, Object> requestMap = new HashMap<>();
+        requestMap.put("gid", requestParam.getGid());
+        requestMap.put("current", requestParam.getCurrent());
+        requestMap.put("size", requestParam.getSize());
+        String resultPageStr = HttpUtil.get("http://localhost:8001/api/shortlink/v1/page", requestMap);
+
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+}
---
+++ admin/src/main/java/com/xmon/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
@@ -0,0 +1,17 @@
+package com.xmon.shortlink.admin.remote.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xmon.shortlink.project.dao.entity.ShortLinkDO;
+import lombok.Data;
+
+/**
+ * 短链接分页请求参数
+ */
+@Data
+public class ShortLinkPageReqDTO extends Page<ShortLinkDO> {
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+}
