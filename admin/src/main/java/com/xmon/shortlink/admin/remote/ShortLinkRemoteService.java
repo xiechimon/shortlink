5f9eb6320d050c54cf8557b5d39cb922fe5998fb
xiechimon <xiechimon@qq.com>
2026-04-14 17:01:56 +0800

    refactor(remote): 抽离短链接口基地址

--- admin/src/main/java/com/xmon/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/xmon/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -8,20 +8,24 @@ import com.xmon.shortlink.admin.common.convention.result.Result;
 import com.xmon.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.xmon.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import com.xmon.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.xmon.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.xmon.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * 管理端调用中台短链接能力的远程服务接口。
  */
 public interface ShortLinkRemoteService {
 
+    String SHORT_LINK_SERVICE_BASE_URL = "http://localhost:8001/api/shortlink/v1";
+
     /**
      * 调用中台的短链接创建接口。
      */
     default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam) {
-        String resultBodyStr = HttpUtil.post("http://localhost:8001/api/shortlink/v1/create", JSON.toJSONString(requestParam));
+        String resultBodyStr = HttpUtil.post(SHORT_LINK_SERVICE_BASE_URL + "/create", JSON.toJSONString(requestParam));
         return JSON.parseObject(resultBodyStr, new TypeReference<>() {
         });
     }
@@ -34,7 +38,19 @@ public interface ShortLinkRemoteService {
         requestMap.put("gid", requestParam.getGid());
         requestMap.put("current", requestParam.getCurrent());
         requestMap.put("size", requestParam.getSize());
-        String resultPageStr = HttpUtil.get("http://localhost:8001/api/shortlink/v1/page", requestMap);
+        String resultPageStr = HttpUtil.get(SHORT_LINK_SERVICE_BASE_URL + "/page", requestMap);
+
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 查询短链接分组总量
+     */
+    default Result<List<ShortLinkGroupCountQueryRespDTO>> listGroupShortLinkCount(List<String> requestParam) {
+        HashMap<String, Object> requestMap = new HashMap<>();
+        requestMap.put("requestParam", requestParam);
+         String resultPageStr = HttpUtil.get(SHORT_LINK_SERVICE_BASE_URL + "/count", requestMap);
 
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
         });
--- project/src/main/java/com/xmon/shortlink/project/dto/resp/ShortLinkCreateRespDTO.java
+++ project/src/main/java/com/xmon/shortlink/project/dto/resp/ShortLinkCreateRespDTO.java
@@ -1,4 +1,4 @@
-package com.xmon.shortlink.project.dto.resp;
+package com.xmon.shortlink.project.remote.dto.resp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
